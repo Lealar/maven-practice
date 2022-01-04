397aad03ef69ef669f222d8fdecefd40ce465332
Mesnyankin Vladislav <goosts@mail.ru>
2022-01-04 13:28:37 +0300

    Initial commit

---
+++ src/main/java/ru/Mesnyankin/Vladislav/App.java
@@ -0,0 +1,17 @@
+package ru.Mesnyankin.Vladislav;
+
+
+/**
+ * Its java doc.
+ * Im good in Maven
+ * Hello world!
+ * @author Vladislav Mesnyankin
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println(org.apache.commons.lang3.text.WordUtils.capitalize("text"));
+    }
+}
